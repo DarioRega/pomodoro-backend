0b766939abe539c572376ece1f05631b2defff33
Renato Moor <renatomoor1@gmail.com>
2021-05-20 23:23:17 +0200

    New: Add Initial Actions

---
+++ app/Actions/Pomodoro/Steps/StartStep.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Actions\Pomodoro\Steps;
+
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class StartStep
+{
+    use AsAction;
+
+    public function handle()
+    {
+        // ...
+    }
+}
