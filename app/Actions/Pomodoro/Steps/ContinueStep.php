c6b500cac2ad01367c14f1d4bfae0dbbc9659879
Renato Moor <renatomoor1@gmail.com>
2021-05-22 09:59:47 +0200

    Add continue step action

---
+++ app/Actions/Pomodoro/Steps/ContinueStep.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Actions\Pomodoro\Steps;
+
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class ContinueStep
+{
+    use AsAction;
+
+    public function handle()
+    {
+        // ...
+    }
+}
