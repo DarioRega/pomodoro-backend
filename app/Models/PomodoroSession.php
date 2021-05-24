330642cd967535447f635efbc37c9d9fe3ed5a5a
Renato Moor <renatomoor1@gmail.com>
2021-05-24 10:53:24 +0200

    Add model documentation

--- app/Models/PomodoroSession.php
+++ app/Models/PomodoroSession.php
@@ -2,6 +2,9 @@
 
 namespace App\Models;
 
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Eloquent;
 use Illuminate\Database\Eloquent\Builder;
