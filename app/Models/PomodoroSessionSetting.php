d4b5ec78e193664b6d6404b444a4828b6bca892b
Renato Moor <renatomoor1@gmail.com>
2021-05-25 19:17:32 +0200

    remove useless last() function

--- app/Models/PomodoroSessionSetting.php
+++ app/Models/PomodoroSessionSetting.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Eloquent;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Carbon;
 
