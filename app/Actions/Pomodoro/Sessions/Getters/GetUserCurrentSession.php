7ce87478f49d46dbe12212ce460e114494c9463f
Renato Moor <renatomoor1@gmail.com>
2021-06-09 22:40:00 +0200

    Finish step when end_time is old

--- app/Actions/Pomodoro/Sessions/Getters/GetUserCurrentSession.php
+++ app/Actions/Pomodoro/Sessions/Getters/GetUserCurrentSession.php
@@ -2,8 +2,11 @@
 
 namespace App\Actions\Pomodoro\Sessions\Getters;
 
+use App\Actions\Pomodoro\Steps\UserActions\FinishStep;
 use App\Enums\SessionStatus;
+use App\Enums\StepStatus;
 use App\Models\PomodoroSession;
+use App\Models\Step;
 use App\Models\User;
 use Illuminate\Http\Response;
 use Lorisleiva\Actions\Concerns\AsAction;
@@ -15,11 +18,27 @@ class GetUserCurrentSession
     public function handle(User $user): ?PomodoroSession
     {
         $sessions = PomodoroSession::byUser($user)->get();
-        return $sessions->filter(function (PomodoroSession $session) {
+
+        $session = $sessions->filter(function (PomodoroSession $session) {
             $isInProgress = $session->status == SessionStatus::IN_PROGRESS;
             $isPaused = $session->status == SessionStatus::PAUSED;
             return $isInProgress || $isPaused ;
         })->first();
+
+        if ($session !== null) {
+            $this->checkIfStepIsFinish($session->current_step);
+        }
+
+        return $session->fresh();
+    }
+
+    private function checkIfStepIsFinish(Step $currentStep)
+    {
+        if (StepStatus::IN_PROGRESS()->is($currentStep->status)) {
+            if ($currentStep->end_time <= now()) {
+                FinishStep::run($currentStep);
+            }
+        }
     }
 
     public function asController(): PomodoroSession|Response
